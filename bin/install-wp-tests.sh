59e913b5ad3bb4568a823624a18c8706a1548694
miya0001 <miya@wpist.me>
2015-05-09 13:40:54 +0900

    bug fix

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -92,7 +92,6 @@ install_db() {
 	fi
 
 	# create database
-	mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA
 	echo "CREATE DATABASE ${DB_NAME} DEFAULT CHARACTER SET ${WP_DB_CHARSET} COLLATE ${WP_DB_COLLATE};" | mysql -u $DB_USER --password="$DB_PASS"
 }
 
