e8a09cb2de482c58e71bd1b37724b813443664db
miya0001 <miya@wpist.me>
2015-05-09 12:24:05 +0900

    fix problem of the unit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,10 +7,4 @@ if ( ! $_tests_dir ) {
 
 require_once $_tests_dir . '/includes/functions.php';
 
-function _manually_load_plugin() {
-	//require dirname( __FILE__ ) . '/../.php';
-}
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
-
 require $_tests_dir . '/includes/bootstrap.php';
-
