f4a70bb5621fbfffe2ec20fcd1c205d4d3f09e55
miya0001 <miya@wpist.me>
2015-05-09 14:51:34 +0900

    some fix

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -2,9 +2,22 @@
 
 class SampleTest extends WP_UnitTestCase {
 
-	function test_sample() {
+	function test_sample()
+	{
 		// replace this with some actual testing code
 		$this->assertTrue( true );
 	}
-}
 
+	/**
+	 * @test
+	 */
+	function check_db_charset()
+	{
+		global $wpdb;
+
+		$sql = 'show table status from wordpress_test';
+		$result = $wpdb->get_results($sql);
+
+		var_dump( $result );
+	}
+}
