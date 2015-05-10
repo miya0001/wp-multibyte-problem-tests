8935343266db048413a26e39e284160c3a756f27
miya0001 <miya@wpist.me>
2015-05-10 15:00:00 +0900

    some fix

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -2,30 +2,27 @@
 
 class SampleTest extends WP_UnitTestCase
 {
-	public function setup()
-	{
-		define( 'DB_CHARSET', 'utf8' );
-		define( 'DB_COLLATE', 'utf8_general_ci' );
-	}
-
 	/**
 	 * @test
 	 */
 	function utf8_testing()
 	{
-		// global $wpdb;
-		// $this->assertSame( $wpdb->charset, 'utf8' );
+		$char = array(
+			'utf8' => 'UTF-8',
+			'ujis' => 'EUC-JP',
+		);
 
+		// $this->assertSame( $_ENV['WP_DB_CHARSET'], $wpdb->charset );
 		$post_id = $this->factory->post->create( array(
-			'post_title' => '日本語のタイトル',
-			'post_content' => 'こんにちは！世界！',
+			'post_title' => mb_convert_encoding( 'こんにちは！', $char[ $_SERVER['WP_DB_CHARSET'] ], 'utf8' ),
+			'post_content' => mb_convert_encoding( '世界へようこそ', $char[ $_SERVER['WP_DB_CHARSET'] ], 'utf8' ),
 		) );
 
 		$this->assertTrue( 0 < $post_id );
 
 		$post = get_post( $post_id );
 
-		$this->assertSame( $post->post_title, '日本語のタイトル' );
-		$this->assertSame( $post->post_content, 'こんにちは！世界！' );
+		$this->assertSame( $post->post_title, 'こんにちは！' );
+		$this->assertSame( $post->post_content, '世界へようこそ' );
 	}
 }
