c59b155aba2313c0890f3109dcdf3682c2e02144
miya0001 <miya@wpist.me>
2015-05-10 14:30:01 +0900

    some fix

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -2,40 +2,23 @@
 
 class SampleTest extends WP_UnitTestCase
 {
+	public function setup()
+	{
+		define( 'DB_CHARSET', 'utf8' );
+		define( 'DB_COLLATE', 'utf8_general_ci' );
+	}
+
 	/**
 	 * @test
 	 */
 	function utf8_testing()
 	{
 		global $wpdb;
-		$wpdb->charset = 'utf8';
-		$wpdb->collate = 'utf8_general_ci';
+		$this->assertSame( $wpdb->charset, 'utf8' );
 
 		$post_id = $this->factory->post->create( array(
 			'post_title' => '日本語のタイトル',
 			'post_content' => 'こんにちは！世界！',
-		) );
-
-		$this->assertTrue( 0 < $post_id );
-
-		$post = get_post( $post_id );
-
-		$this->assertSame( $post->post_title, '日本語のタイトル' );
-		$this->assertSame( $post->post_content, 'こんにちは！世界！' );
-	}
-
-	/**
-	 * @test
-	 */
-	function ujis_testing()
-	{
-		global $wpdb;
-		$wpdb->charset = 'ujis';
-		$wpdb->collate = 'ujis_japanese_ci';
-
-		$post_id = $this->factory->post->create( array(
-			'post_title' => mb_convert_encoding( '日本語のタイトル', 'EUC-JP', 'UTF-8' ),
-			'post_content' => mb_convert_encoding( 'こんにちは！世界！', 'EUC-JP', 'UTF-8' ),
 		) );
 
 		$this->assertTrue( 0 < $post_id );
